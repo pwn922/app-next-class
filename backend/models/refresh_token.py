be5893218dbc51fa559808385dd897cca45876ba
pwn922 <>
2025-04-07 18:35:14 -0700

    fix: refresh model

--- backend/models/refresh_token.py
+++ backend/models/refresh_token.py
@@ -6,9 +6,9 @@ from sqlalchemy.orm import relationship
 class RefreshToken(Base):
     __tablename__ = 'refresh_tokens'
     
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4())
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     token = Column(String(500), nullable=False, unique=True)
-    user_google_id = Column(String(255), ForeignKey('users.google_id'), nullable=False)
+    user_google_id = Column(String(255), ForeignKey('user.google_id'), nullable=False)
     user = relationship("User", backref="refresh_tokens")
     
     def __repr__(self):
