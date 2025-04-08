f791eb4f5bb2e618d50559ecf3d58330e354ca4f
pwn922 <>
2025-04-07 17:55:23 -0700

    upd: user models

--- backend/models/user.py
+++ backend/models/user.py
@@ -12,12 +12,9 @@ class User(Base):
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4())
     google_id = Column(String(255), unique=True, nullable=False)
     email = Column(String(120), unique=True, nullable=False)
-    password = Column(String(255), nullable =False)
     created_at = Column(DateTime, default=datetime.now(timezone.utc), nullable=False)
-
-    #Relacion entre el Usuario y el Horario
-    schedules = relationship('Schedule', back_populates='user', cascade="all, delete-orphan")
     #role = Column(String(50), nullable=False)
+    schedules = relationship('Schedule', back_populates='user', cascade="all, delete-orphan")
 
     def __repr__(self):
         return f'<User {self.email}>'
