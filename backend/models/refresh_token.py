a0bb6999d2bc4fe1c922aaf2c396b7a02bfdc341
pwn922 <>
2025-04-12 19:08:45 -0700

    upd: change user_google_id to uiser_oidc_sub

--- backend/models/refresh_token.py
+++ backend/models/refresh_token.py
@@ -3,13 +3,17 @@ from sqlalchemy import UUID, Column, ForeignKey, String
 from models.base import Base
 from sqlalchemy.orm import relationship
 
+
 class RefreshToken(Base):
     __tablename__ = 'refresh_tokens'
-    
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     token = Column(String(500), nullable=False, unique=True)
-    user_google_id = Column(String(255), ForeignKey('user.google_id'), nullable=False)
+    user_oidc_sub = Column(
+        String(255),
+        ForeignKey('user.oidc_sub'),
+        nullable=False
+    )
     user = relationship("User", backref="refresh_tokens")
-    
+
     def __repr__(self):
         return f'<RefreshToken email={self.email}>'
