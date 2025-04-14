4a86e3aa25e47923e3bc64e292790eef7289d937
pwn922 <>
2025-04-13 20:39:59 -0700

    upd: ConfigGoogle removed

--- backend/config/config.py
+++ backend/config/config.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 
+
 class ConfigFlask:
     load_dotenv()
 
@@ -28,21 +29,27 @@ class ConfigFlask:
 
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', os.urandom(24))
     JWT_ALGORITHMS = os.getenv('JWT_ALGORITHMS', 'RS256').split(",")
-    
 
+"""
 class ConfigGoogle:
     load_dotenv()
 
     GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID")
     GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET")
+    GOOGLE_REDIRECT_URI = os.environ.get("GOOGLE_REDIRECT_URI")
 
     print(GOOGLE_CLIENT_ID)
     print(GOOGLE_CLIENT_SECRET)
+    print(GOOGLE_REDIRECT_URI)
     if not GOOGLE_CLIENT_ID:
         print("Error: GOOGLE_CLIENT_ID is not configured.")
         sys.exit(1)
 
     if not GOOGLE_CLIENT_SECRET:
+        print("Error: GOOGLE_CLIENT_SECRET is not configured.")
+        sys.exit(1)
+
+    if not GOOGLE_REDIRECT_URI:
         print("Error: GOOGLE_CLIENT_SECRET is not configured.")
         sys.exit(1)
 
@@ -52,3 +59,4 @@ class ConfigGoogle:
         "https://www.googleapis.com/auth/userinfo.email",
         "https://www.googleapis.com/auth/userinfo.profile",
     ]
+"""
