86d01b8a052cfe62a490b93f73ab6258513b4b92
pwn922 <mauricio41533@gmail.com>
2025-04-19 22:38:43 -0800

    upd: config, jwt, resources added

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,18 +1,30 @@
+from flask_jwt_extended import JWTManager
 import pytest
 from flask import Flask
 from flask_restful import Api
-from resources.pavilion import PavilionResource, PavilionListResource
-from tests.mocks.pavilion_mock import create_mock_pavilion, create_mock_pavilion_list
+from config.config import ConfigFlask
+from resources.login import LoginResource
+from resources.logout import LogoutResource
+from resources.pavilion import PavilionByNameResource, PavilionResource, PavilionListResource
+from resources.register import RegisterResource
+from resources.schedules import ScheduleListResource, ScheduleResource
 
 @pytest.fixture
 def app():
     """Create test flask app with test config"""
     app = Flask(__name__)
+    app.config.from_object(ConfigFlask)
     app.config['TESTING'] = True
     app.config['DEBUG'] = False
-    api = Api(app)
-    api.add_resource(PavilionListResource, '/api/v1/pavilions/')
-    api.add_resource(PavilionResource, '/api/v1/pavilions/<uuid:id>')
+    jwt = JWTManager(app)
+    api = Api(app, prefix='/api/v1')
+    api.add_resource(LoginResource, '/login')
+    api.add_resource(LogoutResource, '/logout')
+    api.add_resource(PavilionResource, '/pavilions/<string:id>')
+    api.add_resource(PavilionByNameResource, '/pavilions/<string:name>')
+    api.add_resource(PavilionListResource, '/pavilions/')
+    api.add_resource(RegisterResource, '/register')
+    api.add_resource(ScheduleListResource, '/user/schedules/')
     return app
 
 @pytest.fixture
