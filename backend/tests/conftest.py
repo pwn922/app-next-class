54a1a73559eddcf3a0725b369a3d8131596ace00
pwn922 <mauricio41533@gmail.com>
2025-04-19 23:07:59 -0800

    upd: using the same create_app function

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,33 +1,10 @@
-from flask_jwt_extended import JWTManager
 import pytest
-from flask import Flask
-from flask_restful import Api
-from config.config import ConfigFlask
-from resources.login import LoginResource
-from resources.logout import LogoutResource
-from resources.pavilion import PavilionByNameResource, PavilionResource, PavilionListResource
-from resources.register import RegisterResource
-from resources.schedules import ScheduleListResource, ScheduleResource
+from app import create_app
 
 @pytest.fixture
 def app():
-    """Create test flask app with test config"""
-    app = Flask(__name__)
-    app.config.from_object(ConfigFlask)
-    app.config['TESTING'] = True
-    app.config['DEBUG'] = False
-    jwt = JWTManager(app)
-    api = Api(app, prefix='/api/v1')
-    api.add_resource(LoginResource, '/login')
-    api.add_resource(LogoutResource, '/logout')
-    api.add_resource(PavilionResource, '/pavilions/<string:id>')
-    api.add_resource(PavilionByNameResource, '/pavilions/<string:name>')
-    api.add_resource(PavilionListResource, '/pavilions/')
-    api.add_resource(RegisterResource, '/register')
-    api.add_resource(ScheduleListResource, '/user/schedules/')
-    return app
+    return create_app(testing=True)
 
 @pytest.fixture
 def client(app):
-    """Create test client"""
     return app.test_client()
